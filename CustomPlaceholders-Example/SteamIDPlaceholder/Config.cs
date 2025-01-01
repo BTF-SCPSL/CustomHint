3da42ab384c79b766288e75faf64fce4333fb048
Narin <65040937+Narin4ik@users.noreply.github.com>
2025-01-01 19:07:54 +0200

    Add files via upload

---
+++ CustomPlaceholders-Example/SteamIDPlaceholder/Config.cs
@@ -0,0 +1,10 @@
+﻿using Exiled.API.Interfaces;
+
+namespace SteamIDPlaceholder
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+    }
+}
