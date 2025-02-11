da8563f007cbf11e1ce00441d46ea11e25cf9408
Narin <65040937+Narin4ik@users.noreply.github.com>
2025-02-11 18:53:11 +0200

    Update AssemblyInfo.cs

--- CustomHint/Properties/AssemblyInfo.cs
+++ CustomHint/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Back To Future")]
 [assembly: AssemblyProduct("CustomHint")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright ©  2024-2025")]
 [assembly: AssemblyTrademark("BTF")]
 [assembly: AssemblyCulture("")]
 
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.4.1.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
