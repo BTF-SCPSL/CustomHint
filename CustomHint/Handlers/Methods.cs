4782c213774d3fdf53449bd9806a5919a29b38bb
Narin <65040937+Narin4ik@users.noreply.github.com>
2025-02-11 18:48:26 +0200

    [v1.5.0-beta.1] Update Methods.cs

--- CustomHint/Handlers/Methods.cs
+++ CustomHint/Handlers/Methods.cs
@@ -92,17 +92,17 @@ namespace CustomHint.Handlers
 
             if (roundDuration.TotalSeconds <= 60)
                 return translationroundtime["seconds"]
-                    .Replace("{round_duration_seconds}", roundDuration.Seconds.ToString());
+                    .Replace("{round_duration_seconds}", roundDuration.Seconds.ToString("D2"));
 
             if (roundDuration.TotalMinutes < 60)
                 return translationroundtime["minutes"]
-                    .Replace("{round_duration_minutes}", roundDuration.Minutes.ToString())
-                    .Replace("{round_duration_seconds}", roundDuration.Seconds.ToString());
+                    .Replace("{round_duration_minutes}", roundDuration.Minutes.ToString("D2"))
+                    .Replace("{round_duration_seconds}", roundDuration.Seconds.ToString("D2"));
 
             return translationroundtime["hours"]
-                .Replace("{round_duration_hours}", roundDuration.Hours.ToString())
-                .Replace("{round_duration_minutes}", roundDuration.Minutes.ToString())
-                .Replace("{round_duration_seconds}", roundDuration.Seconds.ToString());
+                .Replace("{round_duration_hours}", roundDuration.Hours.ToString("D2"))
+                .Replace("{round_duration_minutes}", roundDuration.Minutes.ToString("D2"))
+                .Replace("{round_duration_seconds}", roundDuration.Seconds.ToString("D2"));
         }
 
         public struct RoleCounts
